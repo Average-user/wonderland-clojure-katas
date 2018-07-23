decd8e37090c8727144543fa61f09810e69719d6
Average-user <lucaspolymeris@pm.me>
2018-07-23 17:53:58 -0400

    wonderland number solved

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,9 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [a b]
+  (= (sort (str a)) (sort (str b))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (letfn [(f [x] (every? #(same-digits? x %) (map #(* x %) [2 3 4 5 6])))]
+    (first (filter f (range 100000 1000000)))))
+  
