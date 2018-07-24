9944d360cea993d173e65018dcf2b2013c71e947
NereaNerealucasyNerealucasyLucas <lucaspolymeris@pm.me>
2018-07-23 20:35:17 -0400

    doublets and magic square solved

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,22 @@
                (slurp)
                (read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+(defn difference [a b] (count (filter not (map = a b))))
+
+(defn next [record seen current goal]
+  (if (= current goal)
+    (conj record current)
+    (let [ops (->> words
+                  (filter #(not (contains? seen %)))
+                  (filter #(== (count current) (count %)))
+                  (filter #(= 1 (difference current %)))
+                  (map #(vector % (difference goal %)))
+                  (sort-by second)
+                  (map first))]
+      (if (empty? ops)
+        []
+        (letfn [(f [word] (next (conj record current)
+                                (conj seen current) word goal))]
+          (first (filter (comp not empty?) (map f ops))))))))
+
+(defn doublets [word1 word2] (next [] #{} word1 word2))
--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,8 +1,14 @@
-(ns magic-square.puzzle)
+(ns magic-square.puzzle
+  (:require [clojure.math.combinatorics :as combo]))
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (letfn [(f       [x] (map (partial reduce +) x))
+          (s       [x] (= 1 (count (set (apply concat (map #(% x) [f columns diag]))))))
+          (columns [x] (f (apply mapv vector x)))
+          (diag    [x] (f (partition 3 (map #(get-in (vec (map vec x)) %)
+                                            [[0 0] [1 1] [2 2] [0 2] [1 1] [2 0]]))))]
+   (vec (map vec (partition 3 (first (filter #(s (partition 3 %))
+                                             (combo/permutations values))))))))
+                                        
